2bc868b69067c3c2efc002dd9b94fff7ed41fe01
rhyd <rhyd@blampt.com>
2014-08-20 13:25:31 +0100

    Refactored

--- bin/of-kanban.rb
+++ bin/of-kanban.rb
@@ -40,96 +40,94 @@ class OFKanban
   end
 
   def run
-    if (!parsed_options?)
-      usage_and_exit
-    end
-
+    usage_and_exit unless parsed_options?
     sync
   end
 
   protected
 
-    def parsed_options?
-      opts = OptionParser.new
-      opts.banner = BANNER
-      opts.on('-d', '--due_date [yyyy-mm-dd]', 'Sync available tasks due by [yyyy-mm-dd]') do |date|
-        @options.due = true
-        @options.due_date = parse_date(date || '')
-        puts "due date is #{@options.due_date}"
-      end
-      # opts.on('-c', '--clear-board', 'Remove all cards from Lean Kit board') { @options.clear = true }
-      opts.on('-f', '--flagged', 'Sync flagged and available tasks') { @options.flagged = true }
-      opts.on('-o', '--open-board', 'Open the kanban board') { @options.open = true }
-      opts.on('-c', '--completed', 'Update OmniFocus with done cards from Kanban board') { @options.completed = true }
-      opts.on('-h', '--help', 'Display this screen' ) do
-        puts opts
-        puts
-        puts FORMAT
-        exit(0)
-      end
+  def parsed_options?
+    return false unless (@arguments.size > 0)
 
-      opts.parse!(@arguments) rescue return false
-
-      true
+    opts = OptionParser.new
+    opts.banner = BANNER
+    opts.on('-d', '--due_date [yyyy-mm-dd]', 'Sync available tasks due by [yyyy-mm-dd]') do |date|
+      @options.due = true
+      @options.due_date = parse_date(date || '')
+      puts "due date is #{@options.due_date}"
     end
-
-    def sync
-      task_man = TaskManager.new()
-      board = KanbanBoard.new()
-      tasks = []
-
-      completed_ids = board.read_board()
-
-      if (@options.completed)
-        # puts "#{completed_ids.inspect}"
-        task_man.close_tasks(completed_ids)
-      end
-
-      if (@options.clear)
-        puts "Clearing board disabled"
-        # board.clear_board()
-      end
-
-      if (@options.due)
-        tasks.concat(task_man.tasks_due_by(@options.due_date))
-      end
-
-      if (@options.flagged)
-        tasks.concat(task_man.flagged_tasks())
-      end
-
-      if (@options.open)
-        # puts board.to_json
-        board.read_board()
-       # system("open", board.url)
-      end
-
-      if (tasks.size > 0)
-        puts "Found #{tasks.size.to_s} cards eligible for syncing with Kanban board"
-        board.add_cards(tasks)
-      end
-    end
-
-    def usage_and_exit
-      puts BANNER
+    # opts.on('-c', '--clear-board', 'Remove all cards from Lean Kit board') { @options.clear = true }
+    opts.on('-f', '--flagged', 'Sync flagged and available tasks') { @options.flagged = true }
+    opts.on('-o', '--open-board', 'Open the kanban board') { @options.open = true }
+    opts.on('-c', '--completed', 'Update OmniFocus with done cards from Kanban board') { @options.completed = true }
+    opts.on('-h', '--help', 'Display this screen') do
+      puts opts
       puts
-      puts MORE_INFO
+      puts FORMAT
       exit(0)
     end
 
-    def parse_date(datestring)
-      days = 0
-      if datestring =~ /^\+(\d+)d$/
-        days = (60 * 60 * 24 * $1.to_i)
-        newdate = Time.now + days
-      elsif datestring =~ /^\+(\w+)w$/
-        days = (7 * 60 * 60 * 24 * $1.to_i)
-        newdate = Time.now + days
-      else
-        newdate = Chronic.parse(datestring, {:context => :future, :ambiguous_time_range => 8})
-      end
-      return Date.parse(newdate.strftime('%Y/%m/%d'))
+    opts.parse!(@arguments) rescue return false
+    true
+  end
+
+  def sync
+    task_man = TaskManager.new
+    board = KanbanBoard.new
+    tasks = []
+
+    completed_ids = board.read_board
+
+    if @options.completed
+      # puts "#{completed_ids.inspect}"
+      task_man.close_tasks(completed_ids)
     end
+
+    if @options.clear
+      puts "Clearing board disabled"
+      # board.clear_board()
+    end
+
+    if @options.due
+      tasks.concat(task_man.tasks_due_by(@options.due_date))
+    end
+
+    if @options.flagged
+      tasks.concat(task_man.flagged_tasks)
+    end
+
+    if @options.open
+      # puts board.to_json
+      board.read_board
+      # system("open", board.url)
+    end
+
+    if tasks.size > 0
+      puts "Found #{tasks.size.to_s} cards eligible for syncing with Kanban board"
+      board.add_cards(tasks)
+    end
+  end
+
+  def usage_and_exit
+    puts BANNER
+    puts
+    puts MORE_INFO
+    exit(0)
+  end
+
+  def parse_date(d)
+    days = 0
+    if d =~ /^\+(\d+)d$/
+      days = (60 * 60 * 24 * $1.to_i)
+      new_date = Time.now + days
+    elsif d =~ /^\+(\w+)w$/
+      days = (7 * 60 * 60 * 24 * $1.to_i)
+      new_date = Time.now + days
+    else
+      new_date = Chronic.parse(d, {:context => :future, :ambiguous_time_range => 8})
+    end
+    Date.parse(new_date.strftime('%Y/%m/%d'))
+  end
 end
 
 app = OFKanban.new(ARGV, STDIN)
